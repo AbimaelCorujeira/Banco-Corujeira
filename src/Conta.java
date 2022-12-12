136ec7657ce859eefaede1bda64b75bb0a5c56e7
AbimaelCorujeira <corujeira.abimael@gmail.com>
2022-12-12 16:12:45 -0300

    encapsulamento

--- src/Conta.java
+++ src/Conta.java
@@ -1,5 +1,5 @@
 public class Conta {
-    double saldo;
+    private double saldo;
     int agencia;
     int numero;
     Cliente titular;
@@ -23,5 +23,9 @@ public class Conta {
             return true;
         }
         return false;
+    }
+
+    public double getSaldo() {
+        return saldo;
     }
 }
