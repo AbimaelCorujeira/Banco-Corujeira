a8b3f12cf31e812e7dfaa652f7216924d5a634b8
AbimaelCorujeira <corujeira.abimael@gmail.com>
2022-12-23 23:06:51 -0300

    lambdas

--- src/br/com/corujeira/banco/util/TesteLambda.java
+++ src/br/com/corujeira/banco/util/TesteLambda.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class Teste {
+public class TesteLambda {
     public static void main(String[] args) {
 
         Conta cc1 = new ContaCorrente(22, 33);
@@ -42,25 +42,16 @@ public class Teste {
         lista.add(cc3);
         lista.add(cc4);
 
-        lista.sort(new Comparator<Conta>() {
-                @Override
-                public int compare(Conta c1, Conta c2) {
-                    return Integer.compare(c1.getNumero(), c2.getNumero());
-                }
-            }
-        );
+        //lista.sort((c1, c2) -> Integer.compare(c1.getNumero(), c2.getNumero()));
 
-        Comparator<Conta> comparator = new  Comparator<Conta>() {
-            @Override
-            public int compare(Conta c1, Conta c2) {
+        Comparator<Conta> comparator = (c1, c2) -> {
                 String nomeC1 = c1.getTitular().getNome();
                 String nomeC2 = c2.getTitular().getNome();
                 return nomeC1.compareTo(nomeC2);
-            }
         };
 
-        for (Conta conta : lista) {
-            System.out.println(conta + ", " + conta.getTitular().getNome());
-        }
+        lista.sort(comparator);
+
+        lista.forEach((conta) -> System.out.println(conta + ", " + conta.getTitular().getNome()));
     }
 }
